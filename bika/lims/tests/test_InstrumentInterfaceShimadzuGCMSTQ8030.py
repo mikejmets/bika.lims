b60fac35c9c624eb40fe79435db18c9a92d64417
Lunga Baliwe <lunga001@gmail.com>
2017-04-25 21:39:49 +0200

    BC-4 Continuing with the test

--- bika/lims/tests/test_InstrumentInterfaceShimadzuGCMSTQ8030.py
+++ bika/lims/tests/test_InstrumentInterfaceShimadzuGCMSTQ8030.py
@@ -3,6 +3,7 @@
 # Copyright 2011-2016 by it's authors.
 # Some rights reserved. See LICENSE.txt, AUTHORS.txt.
 
+from DateTime import DateTime
 from Products.CMFPlone.utils import _createObjectByType
 from bika.lims import logger
 from bika.lims.content.analysis import Analysis
@@ -42,27 +43,73 @@ class TestARImports(BikaSimpleTestCase):
         self.addthing(self.client, 'Contact', Firstname='Rita Mohale',
                       Lastname='Mohale')
 
+    def setUp(self):
+        super(TestARImports, self).setUp()
+        login(self.portal, TEST_USER_NAME)
+        clients = self.portal.clients
+        bs = self.portal.bika_setup
+        # @formatter:off
+        self.client = self.addthing(clients, 'Client', title='Happy Hills', ClientID='HH')
+        contact = self.addthing(self.client, 'Contact', Firstname='Rita', Lastname='Mohale')
+        container = self.addthing(bs.bika_containers, 'Container', title='Bottle', capacity="10ml")
+        # Sample
+        sampletype = self.addthing(bs.bika_sampletypes, 'SampleType', title='Water', Prefix='H2O')
+        samplepoint = self.addthing(bs.bika_samplepoints, 'SamplePoint', title='Toilet')
+        priority = self.addthing(bs.bika_arpriorities, 'ARPriority', title='Normal', sortKey=1)
+
+        ar_category = self.addthing(self.portal.bika_setup.bika_analysiscategories, 'AnalysisCategory',
+                      title='Metals')
+        service = self.addthing(bs.bika_analysisservices, 
+                'AnalysisService', 
+                title='Ecoli', 
+                Keyword="ECO",
+                Category=ar_category.UID())
+        batch = self.addthing(self.portal.batches, 'Batch', title='B1')
+        # Create Sample with single partition
+        self.sample1 = self.addthing(self.client, 'Sample', SampleType=sampletype)
+        self.sample2 = self.addthing(self.client, 'Sample', SampleType=sampletype)
+        self.addthing(self.sample1, 'SamplePartition', Container=container)
+        self.addthing(self.sample2, 'SamplePartition', Container=container)
+        # Create an AR
+        self.ar1 = self.addthing(self.client, 'AnalysisRequest', Contact=contact,
+                                Sample=self.sample1, Analyses=[service], SamplingDate=DateTime())
+        # Create a secondary AR - linked to a Batch
+        self.ar2 = self.addthing(self.client, 'AnalysisRequest', Contact=contact,
+                                Sample=self.sample1, Analyses=[service], SamplingDate=DateTime(),
+                                Batch=batch)
+        # Create an AR - single AR on sample2
+        self.ar3 = self.addthing(self.client, 'AnalysisRequest', Contact=contact,
+                                Sample=self.sample2, Analyses=[service], SamplingDate=DateTime())
+        transaction.commit()
     def tearDown(self):
         super(TestARImports, self).setUp()
         login(self.portal, TEST_USER_NAME)
 
 
     def test_BC_4(self):
-        ar_category = self.addthing(self.portal.bika_setup.bika_analysiscategories, 'AnalysisCategory',
-                      title='Metals')
-        a_ser = self.addthing(self.portal.bika_setup.bika_analysisservices, 'AnalysisService',
-                      title='AR1', keyword='TestKeyword',
-                      category=ar_category.UID())
-        sample_type = self.addthing(self.portal.bika_setup.bika_sampletypes, 'SampleType',
-                      title='Water', Prefix='H2O')
-        # SamplePoint
-        #sample_point = self.addthing(self.portal.bika_setup.bika_samplepoints, 'SamplePoint',
-        #              title='Toilet')
-        #sample_point.reindexObject()
-        #ll = self.addthing(self.client, 'AnalysisRequest', 
-        #        Analyses=a_ser.UID(), SampleType=sample_type.UID(),)
         browser = self.getBrowser(loggedIn=True)
         browser.open(self.client.absolute_url())
+        import_url = '%s/%s' % (self.portal.absolute_url, 'import')
+        #TODO Ask Campell for the URL
+        browser.open(import_url))
+        browser.getControl(name="exim").displayValue = ['Shimadzu GCMS-TQ8030 GC/MS/MS']
+        # Import
+        #Import Instrument File     Horiba Jobin-Yvon - ICP  ${PATH_TO_TEST}/files/ICPlimstest.csv
+        #Page should contain        Service keyword Ni221647 not found
+        #Page should contain        Import finished successfully: 1 ARs and 1 results updated
+        #Go to    ${PLONEURL}/clients/client-1/BR-0001-R01/manage_results
+        #Textfield value should be        css=[selector="Result_Al396152"]  0.1337
+        #>>> ctrl = browser.getControl('File Control')
+        #>>> ctrl
+        #<Control name='file-value' type='file'>
+        #>>> verifyObject(interfaces.IControl, ctrl)
+        #True
+        #>>> ctrl.value is None
+        #True
+        #>>> import cStringIO
+        #>>> ctrl.add_file(cStringIO.StringIO('File contents'),
+        #        ...               'text/plain', 'test.txt')
+        #browser.getControl(name="form.button.save").click()
         import pdb; pdb.set_trace()
 
 def test_suite():
