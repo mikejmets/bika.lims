6146ba68b76a2abc314d96b877f59212d571288c
Ramon Bartl <rb@ridingbytes.com>
2017-06-01 11:59:33 +0200

    Moved all bika.lims viewlets to own package

--- bika/lims/browser/analysisrequest/viewlets/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# This file is part of Bika LIMS
-#
-# Copyright 2011-2017 by it's authors.
-# Some rights reserved. See LICENSE.txt, AUTHORS.txt.
-
-from attachments import AttachmentsViewlet  # noqa
