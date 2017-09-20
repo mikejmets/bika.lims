77c6132676cbcc21411a0db232d5b42b095dcce3
Lunga Baliwe <lunga001@gmail.com>
2017-09-20 14:52:32 +0200

    Took Seed Value on the UI and aswell as on the subscriber

--- bika/lims/subscribers/bikasetup.py
+++ bika/lims/subscribers/bikasetup.py
@@ -7,10 +7,8 @@
 
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore import permissions
-from bika.lims.numbergenerator import INumberGenerator
 from bika.lims.permissions import ManageWorksheets
 from bika.lims.permissions import AddClient, EditClient, ManageClients
-from zope.component import getUtility
 
 def BikaSetupModifiedEventHandler(instance, event):
     """ Event fired when BikaSetup object gets modified.
@@ -48,15 +46,3 @@ def BikaSetupModifiedEventHandler(instance, event):
         mp(EditClient, roles, 0)
         mp(permissions.ModifyPortalContent, roles, 0)
         obj.reindexObject()
-
-    #number_generator = getUtility(INumberGenerator)
-    #for rec in instance.getIDFormatting():
-    #    if 'seed' in rec:
-    #        if 'prefix' in rec and 'context' not in rec:
-    #            new_seq = number_generator.set_seed(key=rec['prefix'], seed=rec['seed'])
-    #        elif 'context' in rec:
-    #            new_seq = number_generator.set_seed(key=rec['context'], seed=rec['seed'])
-    #        #index = instance.getIDFormatting().index(rec)
-    #        #del instance.getIDFormatting()[index]['seed']
-    #        import pdb; pdb.set_trace()
-    #        del rec['seed']
