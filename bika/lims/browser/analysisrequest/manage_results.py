b2f373223407094451f1da40b503228efd0f7bff
Lunga Baliwe <lunga001@gmail.com>
2017-10-09 14:56:29 +0200

    BC-179 Started with issue, not working yet

--- bika/lims/browser/analysisrequest/manage_results.py
+++ bika/lims/browser/analysisrequest/manage_results.py
@@ -22,6 +22,7 @@ from plone.app.layout.globals.interfaces import IViewView
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.interface import implements
+from bika.lims.browser import BrowserView
 
 import plone
 
@@ -51,7 +52,13 @@ class AnalysisRequestManageResultsView(AnalysisRequestViewView):
                     t.review_states[0]['transitions'] = [{'id': 'submit'},
                                                          {'id': 'retract'},
                                                          {'id': 'verify'}]
-                    t.show_select_column = True
+                    t.show_select_column = False
+                    t.expand_all_categories= False
+                    t.ajax_categories = True
+                    t.category_index = 'getCategoryTitle'
+                    t.ajax_categories_url = self.context.absolute_url() + \
+                                               "/ajax_ar_manage_expand_category"
+
                     poc_value = POINTS_OF_CAPTURE.getValue(poc)
                     self.tables[poc_value] = t.contents_table()
             # If a general retracted is done, rise a waring
@@ -93,3 +100,13 @@ class AnalysisRequestManageResultsView(AnalysisRequestViewView):
                         "instruments. Results edition not allowed")
             message = "%s: %s" % (message, (', '.join(invalid)))
             self.context.plone_utils.addPortalMessage(message, 'warn')
+
+class AJAXARManageCategoryExpand(BrowserView):
+    def createAnalysesView(self, context, request, **kwargs):
+        return AnalysesView(context, request, **kwargs)
+
+    def __call__(self):
+        if 'ajax_category_expand' in self.request.keys():
+            cat = self.request.get('cat')
+            asv = self.createAnalysesView(self.context, self.request, category=cat)
+            return asv.rendered_items()
