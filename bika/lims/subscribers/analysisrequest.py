6eba87e36a1c15e8d1a69324fc7f304e21f7e64c
Test2 Bika <bika@test.mjlims.com>
2017-10-23 07:09:26 -0700

    Cleaned out unused code

--- bika/lims/subscribers/analysisrequest.py
+++ bika/lims/subscribers/analysisrequest.py
@@ -15,24 +15,4 @@ def ObjectInitializedEventHandler(instance, event):
         return
 
     instance.setDefaultPriority()
-    samplingDate = instance.getSamplingDate()
-    if samplingDate and samplingDate._tz != samplingDate.localZone():
-        samplingDate._tz = samplingDate.localZone()
-        instance.setSamplingDate(samplingDate)
-    return
-
-def ARModifiedEventHandler(instance, event):
-
-    if instance.portal_type != "AnalysisRequest":
-        return
-
-    samplingDate = instance.getSamplingDate()
-    if samplingDate and samplingDate._tz != samplingDate.localZone():
-        samplingDate._tz = samplingDate.localZone()
-        instance.setSamplingDate(samplingDate)
-
-    dateSampled = instance.getDateSampled()
-    if dateSampled and dateSampled._tz != dateSampled.localZone():
-        dateSampled._tz = dateSampled.localZone()
-        instance.setDateSampled(dateSampled)
     return
